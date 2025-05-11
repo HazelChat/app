e7ca758de07191e4cf79e44887864c9301a2e6ab
Makisuo <minetoaster12@gmail.com>
2025-05-12 01:17:57 +0200

    init

--- packages/drizzle/drizzle.config.ts
+++ packages/drizzle/drizzle.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from "drizzle-kit"
 
 export default defineConfig({
 	out: "./drizzle",
-	schema: "./src/drizzle/schema.ts",
+	schema: "./src/schema.ts",
 	dialect: "postgresql",
 	dbCredentials: {
-		url: "postgresql://postgres:mkwqhcfdnqdelwn1@142.132.228.194:6992/zero",
+		url: "postgresql://user:password@127.0.0.1:5430/postgres",
+		// PRODDB
+		// url: "postgresql://postgres:mkwqhcfdnqdelwn1@142.132.228.194:6992/zero",
 	},
 })
