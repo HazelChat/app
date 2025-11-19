5d1c61f4c6113a262a7590b480970376d25b7b2c
Makisuo <minetoaster12@gmail.com>
2025-11-19 01:14:09 +0100

    feat: add progress loader

--- apps/web/src/components/loader.tsx
+++ apps/web/src/components/loader.tsx
@@ -1,12 +1,20 @@
+import { ProgressBar, ProgressBarTrack } from "~/components/ui/progress-bar"
+
 export const Loader = () => {
 	return (
 		<div className="flex h-screen flex-col items-center justify-center gap-6">
-			<div className="mask-radial-at-center mask-radial-from-black mask-radial-to-transparent relative aspect-square w-full max-w-sm">
-				<img
-					src="/images/squirrle_window.png"
-					alt="squirrel"
-					className="mask-size-[110%_90%] mask-linear-to-r mask-from-black mask-to-transparent mask-center mask-no-repeat mask-[url(/images/image-mask.png)] h-full w-full rounded-md bg-center bg-cover bg-no-repeat object-cover"
-				/>
+			<div className="flex w-full max-w-sm flex-col items-center gap-4">
+				<div className="mask-radial-at-center mask-radial-from-black mask-radial-to-transparent relative aspect-square w-full">
+					<img
+						src="/images/squirrle_window.png"
+						alt="squirrel"
+						className="mask-size-[110%_90%] mask-linear-to-r mask-from-black mask-to-transparent mask-center mask-no-repeat mask-[url(/images/image-mask.png)] h-full w-full rounded-md bg-center bg-cover bg-no-repeat object-cover"
+					/>
+				</div>
+
+				<ProgressBar isIndeterminate aria-label="Loading">
+					<ProgressBarTrack />
+				</ProgressBar>
 			</div>
 
 			<p className="font-bold font-mono text-xl">
